b5355443bc9114d3d023d5217eb9cd7e551fec42
Allen Sanabria <asanabria@linuxdynasty.org>
2016-02-17 13:43:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WorkStationSetUp
+Ansible PlayBook to set up my local workstation
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-sudo apt-get install python-dev python-pip git
-sudo pip install ansible
-mkdir -p linuxdynasty
-cd linuxdynasty/workstation
-git clone https://github.com/linuxdynasty/WorkStationSetUp.git workstation
-cd workstation
-ansible-playbook -vvvv -i localhost, -c local linuxdynasty.yml --ask-become-pass linuxdynasty.yml
+sudo apt-get -y install python-dev python-pip git;\
+    sudo pip install ansible;\
+    mkdir -p linuxdynasty;\
+    cd linuxdynasty;\
+    git clone https://github.com/linuxdynasty/WorkStationSetUp.git workstation;\
+    cd workstation;\
+    ansible-playbook -vvvv -i localhost, -c local linuxdynasty.yml --ask-become-pass linuxdynasty.yml
